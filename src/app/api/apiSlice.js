07d6f39f9c0bff7d43ff8c6e2dedd00ae643bc94
Abdul Rehman <abaz77694@gmail.com>
2025-08-26 15:26:07 +0500

    Changes

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://roledash.onrender.com",
+  baseUrl: "https://lesson-1-backend.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
